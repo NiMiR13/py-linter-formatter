e7406ce80c98932708959953ea75499fff09535d
NiMiR13 <r0932665555@gmail.com>
2026-01-28 16:03:03 +0200

    fix: restore one-liner and fix line length

--- app/main.py
+++ app/main.py
@@ -23,7 +23,6 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # Здесь просто вызываем первую функцию для каждого файла, как и просил Люк
     return [
         format_single_linter_file(path, errors)
         for path, errors in linter_report.items()
